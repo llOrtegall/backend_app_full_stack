8052a1eb4480593a47c2c46972f1a8b906dcf44f
llortegall <ivanortega_97@hotmail.com>
2026-01-20 13:44:07 -0500

    feat: implement UserModel class for user persistence with Sequelize

---
+++ backend/src/infrastructure/persistence/model/user.ts
@@ -0,0 +1,38 @@
+import {
+  DataTypes,
+  Model,
+  type InferAttributes,
+  type InferCreationAttributes,
+  type CreationOptional,
+} from "sequelize";
+import { pgConn } from "../connection/pg.ts";
+
+export class UserModel extends Model<
+  InferAttributes<UserModel>,
+  InferCreationAttributes<UserModel>
+> {
+  declare id: string;
+  declare name: string;
+  declare email: string;
+  declare password: string;
+  declare role: string;
+  declare isActive: boolean;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+UserModel.init(
+  {
+    id: { type: DataTypes.UUID, primaryKey: true },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false },
+    isActive: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  },
+  {
+    sequelize: pgConn,
+  },
+);
