cdbac35fc2528f40c317c8bf6079fab80d52e095
llortegall <ivanortega_97@hotmail.com>
2026-01-22 16:43:04 -0500

    feat: enhance product creation route with image upload handling

--- backend/src/presentation/router/product.ts
+++ backend/src/presentation/router/product.ts
@@ -1,21 +1,35 @@
 import { Router } from "express";
+import multer from "multer";
 import { ProductPgRepo } from "../../infrastructure/repositories/product/ProductPgRepo.ts";
 import { ProductUseCases } from "../../application/product/use-cases.ts";
 import { ProductController } from "../controller/product.ts";
+import { R2StorageService } from "../../infrastructure/storage/r2Storage.ts";
 
 const productRouter = Router();
+
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  fileFilter: (_req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only images are allowed"));
+    }
+  },
+});
 
 /**
  * initialize the repositories
  */
-
+const fileStore = new R2StorageService();
 const pgProductRepo = new ProductPgRepo();
 
 /**
  * inicialize use cases
  */
 
-const productUseCases = new ProductUseCases(pgProductRepo);
+const productUseCases = new ProductUseCases(pgProductRepo, fileStore);
 
 /**
  * Initialize the controller
@@ -25,6 +39,10 @@ const productController = new ProductController(productUseCases);
 /**
  * Define routes
  */
-productRouter.post("/", productController.createProductCtrl);
+productRouter.post(
+  "/",
+  upload.single("image"),
+  productController.createProductCtrl,
+);
 
 export { productRouter };
