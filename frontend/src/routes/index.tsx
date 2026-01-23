1ab5760738b2ae441804e78bb060da371f9985da
llortegall <ivanortega_97@hotmail.com>
2026-01-23 15:07:12 -0500

    feat: update routing configuration to include nested routes for products

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,24 @@
+import { lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router";
 
-import NewProductPage from "../pages/NewProduct.tsx";
+const Layout = lazy(() => import("./Layout.tsx"));
+const NewProductPage = lazy(() => import("../pages/NewProduct.tsx"));
+const ProductsPage = lazy(() => import("../pages/Products.tsx"));
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <NewProductPage />,
+    element: <Suspense><Layout /></Suspense>,
+    children: [
+      {
+        path: "/products",
+        element: <Suspense><ProductsPage /></Suspense>,
+      },
+      {
+        path: "/products/new",
+        element: <Suspense><NewProductPage /></Suspense>,
+      }
+    ]
   }
 ]);
 
