4c6917cb18b6539d15cc0626409cd7bbe0f5c7d9
llortegall <ivanortega_97@hotmail.com>
2026-01-23 15:07:26 -0500

    feat: add ProductPage and Layout components with product fetching functionality

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import { StrictMode } from 'react';
 import './index.css';
 
 import { router } from './routes';
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api/v1';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
